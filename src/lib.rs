a9c2faa7302e591f40e2be9f4d0f2c3b03c2ef44
Robey Pointer <robeypointer@gmail.com>
2019-02-12 09:18:11 -0800

    add allocate and iteration. needs work.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-// use core::mem::size_of;
+use core::mem::size_of;
 use core::slice;
 
 #[macro_use]
@@ -8,8 +8,8 @@ extern crate static_assertions;
 pub mod block_colors;
 pub mod free_list;
 
-pub use self::block_colors::{BlockRange, Color, ColorMap};
-pub use self::free_list::{FreeList, FREE_BLOCK_SIZE};
+pub use self::block_colors::{BlockRange, BLOCKS_PER_COLORMAP_BYTE, Color, ColorMap};
+pub use self::free_list::{FreeBlock, FreeBlockPtr, FreeList, FreeListIterator, FREE_BLOCK_SIZE};
 
 /// configurable things:
 /// how many bytes are in each block of memory?
@@ -19,9 +19,6 @@ const BLOCK_SIZE_BYTES: usize = 16;
 // const WORD_SIZE_BITS: usize = WORD_SIZE_BYTES * 8;
 
 // const BLOCK_SIZE_WORDS: usize = BLOCK_SIZE_BYTES / WORD_SIZE_BYTES;
-
-// we need to reserve 2 bits per block for tracking.
-const BLOCKS_PER_METADATA_BYTE: usize = 8 / 2;
 
 
 // block size must be big enough to hold linking info for the free list.
@@ -39,60 +36,200 @@ fn floor_to(n: usize, chunk: usize) -> usize {
     n / chunk * chunk
 }
 
+fn ceil_to(n: usize, chunk: usize) -> usize {
+    div_ceil(n, chunk) * chunk
+}
 
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum SpanType {
+    Color(Color),
+    Free
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct HeapSpan {
+    pub start: *const u8,
+    pub end: *const u8,
+    pub span_type: SpanType,
+}
+
+impl HeapSpan {
+    fn from_free_block(block: &'static FreeBlock) -> HeapSpan {
+        HeapSpan { start: block.start(), end: block.end(), span_type: SpanType::Free }
+    }
+
+    fn from_block_range(heap: &Heap, range: BlockRange) -> HeapSpan {
+        HeapSpan {
+            start: heap.address_of(range.start),
+            end: heap.address_of(range.end),
+            span_type: SpanType::Color(range.color),
+        }
+    }
+}
+
+impl fmt::Debug for HeapSpan {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.span_type {
+            SpanType::Free => write!(f, "FREE"),
+            SpanType::Color(color) => write!(f, "{:?}", color),
+        }?;
+        write!(f, "[{}]", (self.end as usize) - (self.start as usize))
+    }
+}
+
+
 pub struct Heap {
     pool: &'static [u8],
-    metadata: &'static [u8],
+    color_map: ColorMap,
     blocks: usize,
+    pub current_color: Color,
 
-    pub free: FreeList,
+    free_list: FreeList,
 }
 
 impl Heap {
     pub fn new(memory: &'static mut [u8]) -> Heap {
         // total heap = pool + metadata, and pool is just metadata * blocks_per_metadata * block_size
         // so metadata size = heap size / (1 + bpm * bs)
-        let divisor = 1 + BLOCKS_PER_METADATA_BYTE * BLOCK_SIZE_BYTES;
+        let divisor = 1 + BLOCKS_PER_COLORMAP_BYTE * BLOCK_SIZE_BYTES;
         let metadata_size = div_ceil(memory.len(), divisor);
         let pool_size = floor_to(memory.len() - metadata_size, BLOCK_SIZE_BYTES);
-        let (pooldata, metadata) = memory.split_at(memory.len() - metadata_size);
+        let (pooldata, metadata) = memory.split_at_mut(memory.len() - metadata_size);
         let blocks = pool_size / BLOCK_SIZE_BYTES;
 
         // all of memory is free.
         let pool = unsafe { slice::from_raw_parts(pooldata.as_ptr(), pool_size) };
-        Heap { pool, metadata, blocks, free: FreeList::new(pool) }
+        Heap {
+            pool,
+            color_map: ColorMap::new(metadata),
+            blocks,
+            current_color: Color::Blue,
+            free_list: FreeList::new(pool)
+        }
+    }
+
+    // for tests:
+    pub fn from_raw<T>(memory: &mut T) -> Heap {
+        Heap::new(unsafe { &mut *(slice::from_raw_parts_mut(memory as *mut T as *mut u8, size_of::<T>())) })
+    }
+
+    #[inline]
+    fn address_of(&self, block: usize) -> *const u8 {
+        ((self.pool.as_ptr() as usize) + block * BLOCK_SIZE_BYTES) as *const u8
+    }
+
+    #[inline]
+    fn block_of(&self, p: *const u8) -> usize {
+        ((p as usize) - (self.pool.as_ptr() as usize)) / BLOCK_SIZE_BYTES
+    }
+
+    #[inline]
+    fn block_range_of(&self, memory: &'static [u8], color: Color) -> BlockRange {
+        let start = self.block_of(memory.as_ptr());
+        let end = start + memory.len() / BLOCK_SIZE_BYTES;
+        BlockRange { start, end, color }
+    }
+
+    #[inline]
+    fn start(&self) -> *const u8 {
+        self.pool.as_ptr()
+    }
+
+    #[inline]
+    fn end(&self) -> *const u8 {
+        self.address_of(self.blocks)
+    }
+
+    fn get_range(&self, p: *const u8, max: *const u8) -> BlockRange {
+        self.color_map.get_range(self.block_of(p), self.block_of(max))
+    }
+
+    pub fn allocate(&mut self, amount: usize) -> Option<&'static [u8]> {
+        if let Some(memory) = self.free_list.allocate(ceil_to(amount, BLOCK_SIZE_BYTES)) {
+            self.color_map.set_range(self.block_range_of(memory, self.current_color));
+            Some(memory)
+        } else {
+            None
+        }
+    }
+
+    pub fn iter(&self) -> HeapIterator {
+        HeapIterator { heap: self, next_free: self.free_list.first(), current: self.start() }
+    }
+
+    pub fn dump(&self) -> String {
+        self.iter().map(|span| { format!("{:?}", span) }).collect::<Vec<String>>().join(", ")
     }
 }
 
-
-impl fmt::Display for Heap {
+impl fmt::Debug for Heap {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-            "Heap(pool={:?}, blocks={}x{}, metadata={:?}, free=[{:?}])",
-            self.pool as *const _, self.blocks, BLOCK_SIZE_BYTES, self.metadata.len(), self.free
-        )
+        write!(f, "Heap(pool={:?}, blocks={}x{}, ", self.pool as *const _, self.blocks, BLOCK_SIZE_BYTES)?;
+        if f.alternate() {
+            write!(f, "{}", self.dump())?;
+        } else {
+            write!(f, "{:?}, {:?}", self.color_map, self.free_list)?;
+        }
+        write!(f, ")")
     }
 }
+
+
+pub struct HeapIterator<'a> {
+    heap: &'a Heap,
+    next_free: Option<FreeBlockPtr>,
+    current: *const u8,
+}
+
+impl<'a> Iterator for HeapIterator<'a> {
+    type Item = HeapSpan;
+
+    // tricky: there are two lists to traverse in tandem. if the current
+    // pointer is the next in the free list, that takes precedence.
+    // otherwise, use the block map, but don't let it follow a chain past
+    // the next free span.
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current >= self.heap.end() { return None }
+
+        if let Some(free) = self.next_free {
+            if free.block().start() == self.current {
+                self.next_free = free.next();
+                self.current = free.block().end();
+                return Some(HeapSpan::from_free_block(free.block()));
+            }
+        }
+
+        let limit = self.next_free.map(|p| p.block() as *const FreeBlock as *const u8).unwrap_or(self.heap.end());
+        let span = self.heap.get_range(self.current, limit);
+        self.current = self.heap.address_of(span.end);
+        Some(HeapSpan::from_block_range(self.heap, span))
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use crate::Heap;
 
-    static mut DATA256: [u8; 256] = [0; 256];
-
     #[test]
-    fn make() {
-        println!("at {:?}", unsafe { &DATA256 as *const _ });
-        println!("{}", Heap::new(unsafe { &mut DATA256 }));
-
-        let h = Heap::new(unsafe { &mut DATA256 });
-        h.free.list.block_mut().split(32);
-        println!("{}", h);
+    fn new_heap() {
+        let mut data: [u8; 256] = [0; 256];
+        let h = Heap::from_raw(&mut data);
+        assert_eq!(h.start(), &data[0] as *const u8);
+        assert_eq!(h.end(), unsafe { h.start().offset(240) });
+        assert_eq!(h.dump(), "FREE[240]");
     }
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn allocate() {
+        let mut data: [u8; 256] = [0; 256];
+        let mut h = Heap::from_raw(&mut data);
+        let alloc = h.allocate(32);
+        assert!(alloc.is_some());
+        if let Some(memory) = alloc {
+            assert_eq!(memory.len(), 32);
+            assert_eq!(h.dump(), "Blue[32], FREE[208]");
+        }
     }
 }
